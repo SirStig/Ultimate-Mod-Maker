934af62c07f2e839a83a938e946d9cfa6b0faeff
SirStig <twogoodgamer2@gmail.com>
2016-02-01 20:39:47 -0700

    First Stuff

---
+++ Ultimate Mod Maker/Main.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Ultimate_Mod_Maker
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.GDT = new System.Windows.Forms.Button();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SInc = new System.Windows.Forms.Button();
+            this.MC = new System.Windows.Forms.Button();
+            this.quitMain = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GDT
+            // 
+            this.GDT.BackgroundImage = global::Ultimate_Mod_Maker.Properties.Resources.GDT;
+            this.GDT.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.GDT.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.GDT.Location = new System.Drawing.Point(122, 411);
+            this.GDT.Name = "GDT";
+            this.GDT.Size = new System.Drawing.Size(270, 129);
+            this.GDT.TabIndex = 0;
+            this.GDT.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackgroundImage = global::Ultimate_Mod_Maker.Properties.Resources.BackGround;
+            this.pictureBox7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox7.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(1008, 551);
+            this.pictureBox7.TabIndex = 1;
+            this.pictureBox7.TabStop = false;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.AllowNavigation = false;
+            this.webBrowser1.AllowWebBrowserDrop = false;
+            this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            this.webBrowser1.Location = new System.Drawing.Point(122, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(874, 393);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.TabStop = false;
+            this.webBrowser1.Url = new System.Uri("http://ultimatemm.blogspot.com/", System.UriKind.Absolute);
+            this.webBrowser1.WebBrowserShortcutsEnabled = false;
+            // 
+            // SInc
+            // 
+            this.SInc.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("SInc.BackgroundImage")));
+            this.SInc.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.SInc.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.SInc.Location = new System.Drawing.Point(425, 411);
+            this.SInc.Name = "SInc";
+            this.SInc.Size = new System.Drawing.Size(270, 129);
+            this.SInc.TabIndex = 2;
+            this.SInc.UseVisualStyleBackColor = true;
+            // 
+            // MC
+            // 
+            this.MC.BackgroundImage = global::Ultimate_Mod_Maker.Properties.Resources.MC;
+            this.MC.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.MC.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.MC.Location = new System.Drawing.Point(726, 411);
+            this.MC.Name = "MC";
+            this.MC.Size = new System.Drawing.Size(270, 129);
+            this.MC.TabIndex = 3;
+            this.MC.UseVisualStyleBackColor = true;
+            // 
+            // quitMain
+            // 
+            this.quitMain.BackColor = System.Drawing.Color.Black;
+            this.quitMain.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("quitMain.BackgroundImage")));
+            this.quitMain.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.quitMain.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.quitMain.Location = new System.Drawing.Point(0, 449);
+            this.quitMain.Name = "quitMain";
+            this.quitMain.Size = new System.Drawing.Size(116, 102);
+            this.quitMain.TabIndex = 4;
+            this.quitMain.UseVisualStyleBackColor = false;
+            this.quitMain.Click += new System.EventHandler(this.quitApp);
+            // 
+            // Main
+            // 
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1008, 551);
+            this.Controls.Add(this.quitMain);
+            this.Controls.Add(this.MC);
+            this.Controls.Add(this.SInc);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.GDT);
+            this.Controls.Add(this.pictureBox7);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "Ultimate Mod Maker";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GDT;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button SInc;
+        private System.Windows.Forms.Button MC;
+        private System.Windows.Forms.Button quitMain;
+    }
+}
+
---
+++ Ultimate Mod Maker/Main.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic;
+
+namespace Ultimate_Mod_Maker
+{
+
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            //Basic Init
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Handles what happens when the app is loaded
+        }
+
+        /*//Handles settings button being pushed
+        private void settingsPushed(object sender, EventArgs e)
+        {
+            if (settings.Visible.Equals(false))
+            {
+                settings.Show();
+            }
+            else
+            {
+                settings.Hide();
+            }
+
+        }*/
+
+        //If quit is clicked the app closes
+        private void quitApp(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }/*
+
+        //Shows the GDT Menu
+        private void gdtShowHide(object sender, EventArgs e)
+        {
+            if (GDT.Visible.Equals(false))
+            {
+                GDT.Show();
+            }
+            else
+            {
+                GDT.Hide();
+            }
+        }
+
+        //Shows the topic menu
+        private void topicPanelShowHide(object sender, EventArgs e)
+        {
+            if (topicPanel.Visible.Equals(false))
+            {
+                topicPanel.Show();
+            }
+            else
+            {
+                topicPanel.Hide();
+            }
+        }
+
+        //!!!!!Main Var for all the mods!!!!!
+        string mods = "";
+
+        //Creates the Variales for topic
+        double actionV = 0.6;
+        double adventureV = 0.6;
+        double rpgV = 0.6;
+        double simulationV = 0.6;
+        double strategyV = 0.6;
+        double casualV = 0.6;
+
+        double youngV = 0.6;
+        double everyoneV = 0.6;
+        double matureV = 0.6;
+
+        string topicNameString = "Random Topic";
+
+        // Handles Topic Making
+
+        private void actionValueSet(object sender, EventArgs e)
+        {
+            // Checks to see what the action bar value equals then converts it for GDT modding
+            if (actionBar.Value == 1)
+            {
+                actionV = 0.3;
+            }
+            else if (actionBar.Value == 2)
+            {
+                actionV = 0.6;
+            }
+            else
+            {
+                actionV = 1;
+            }
+        }
+
+        private void adventureValueSet(object sender, EventArgs e)
+        {
+            // Checks to see what the adventure bar value equals then converts it for GDT modding
+            if (adventureBar.Value == 1)
+            {
+                adventureV = 0.3;
+            }
+            else if (adventureBar.Value == 2)
+            {
+                adventureV = 0.6;
+            }
+            else
+            {
+                adventureV = 1;
+            }
+        }
+
+        private void rpgValueSet(object sender, EventArgs e)
+        {
+            // Checks to see what the rpg bar value equals then converts it for GDT modding
+            if (rpgBar.Value == 1)
+            {
+                rpgV = 0.3;
+            }
+            else if (rpgBar.Value == 2)
+            {
+                rpgV = 0.6;
+            }
+            else
+            {
+                rpgV = 1;
+            }
+        }
+
+        private void simulationValueSet(object sender, EventArgs e)
+        {
+
+            // Checks to see what the simulation bar value equals then converts it for GDT modding
+            if (simulationBar.Value == 1)
+            {
+                simulationV = 0.3;
+            }
+            else if (simulationBar.Value == 2)
+            {
+                simulationV = 0.6;
+            }
+            else
+            {
+                simulationV = 1;
+            }
+        }
+
+        private void strategyValueSet(object sender, EventArgs e)
+        {
+            // Checks to see what the stratgey bar value equals then converts it for GDT modding
+            if (strategyBar.Value == 1)
+            {
+                strategyV = 0.3;
+            }
+            else if (strategyBar.Value == 2)
+            {
+                strategyV = 0.6;
+            }
+            else
+            {
+                strategyV = 1;
+            }
+        }
+
+        private void casualValueSet(object sender, EventArgs e)
+        {
+            // Checks to see what the casual bar value equals then converts it for GDT modding
+            if (casualBar.Value == 1)
+            {
+                casualV = 0.3;
+            }
+            else if (casualBar.Value == 2)
+            {
+                casualV = 0.6;
+            }
+            else
+            {
+                casualV = 1;
+            }
+        }
+
+        private void youngValueSet(object sender, EventArgs e)
+        {
+            if (young.Text == "Bad")
+            {
+                youngV = 0.3;
+            }
+            else if (young.Text == "Good")
+            {
+                youngV = 0.6;
+            }
+            else
+            {
+                youngV = 1;
+            }
+        }
+
+        private void everyoneValueSet(object sender, EventArgs e)
+        {
+            if (everyone.Text == "Bad")
+            {
+                everyoneV = 0.3;
+            }
+            else if (everyone.Text == "Good")
+            {
+                everyoneV = 0.6;
+            }
+            else
+            {
+                everyoneV = 1;
+            }
+        }
+
+        private void matureValueSet(object sender, EventArgs e)
+        {
+            if (mature.Text == "Bad")
+            {
+                matureV = 0.3;
+            }
+            else if (mature.Text == "Good")
+            {
+                matureV = 0.6;
+            }
+            else
+            {
+                matureV = 1;
+            }
+        }
+
+        private void topicChangeName(object sender, EventArgs e)
+        {
+            //Changes the topic name if the topic textbox has changed text
+            topicNameString = topicName.Text;
+        }
+
+        //Adds the topic to the mods string if the Add Topic button is pushed
+        private void addTopicToMods(object sender, EventArgs e)
+        {
+            mods = mods + "\n" + "	" + gdtModNameString + ".addTopic = function () {\n" + "		" + "GDT.addTopics([ \n" + "		{" + "			" + "id: \"" + topicNameString + "\",\n" + "			" + "name: \"" + topicNameString + "\".localize(\"game topic\"),\n" + "			" + "genreWeightings: [" + actionV + ", " + adventureV + ", " + rpgV + ", " + simulationV + ", " + strategyV + ", " + casualV + "]," + "			" + "audienceWeightings: [" + youngV + ", " + everyoneV + ", " + matureV + "]\n" + "		}\n" + "		]);\n" + "	};";
+        }
+
+        //Handles the GDT mod naming proccess
+
+
+        string gdtModNameString = "Example";
+
+        private void changeGDTModName(object sender, EventArgs e)
+        {
+            gdtModNameString = gdtModName.Text;
+        }
+
+        //Generates the mod file
+        //Currently only generates the topic and the basic folder and stuff.
+
+        private void finishClicked(object sender, EventArgs e)
+        {
+
+        }
+        */
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /*private void saveDialogOK(object sender, CancelEventArgs e)
+        {
+            string FolderName = gdtModNameString;
+            string folderPath = System.IO.Path.GetDirectoryName(saveGDTFile.FileName);
+            string modFile = "var." + gdtModNameString + " = {}; \n (function () { \n" + mods + "\n})();";
+            System.IO.Directory.CreateDirectory(folderPath);
+        }*/
+
+    }
+
+}
